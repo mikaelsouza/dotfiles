b86ea90fd8fe04033d7faf079849749227129d87
Mikael Souza <mikael.souza.cc@gmail.com>
2023-08-12 17:43:36 -0400

    new changes to the dotfile

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- map leader
+-- leader mapping
 vim.g.mapleader = " "
 
 -- bootstrap lazy.nvim
@@ -17,23 +17,64 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
+-- lazy plugin setup
+require("lazy").setup {
+    {"rose-pine/neovim", name = "rose-pine"},
     {
-        {"folke/tokyonight.nvim"},
-        {
-            "VonHeikemen/lsp-zero.nvim",
-            branch = "v2.x",
-            dependencies = {
-                {"neovim/nvim-lspconfig"},
-                {"hrsh7th/nvim-cmp"},
-                {"hrsh7th/cmp-nvim-lsp"},
-                {"L3MON4D3/LuaSnip"}
-            }
-        },
-        {"vim-autoformat/vim-autoformat"}
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
+        dependencies = {
+            {"neovim/nvim-lspconfig"},
+            {"hrsh7th/nvim-cmp"},
+            {"hrsh7th/cmp-nvim-lsp"},
+            {"L3MON4D3/LuaSnip"}
+        }
+    },
+    {"vim-autoformat/vim-autoformat"},
+    {"nvim-treesitter/nvim-treesitter"},
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.2",
+        dependencies = {
+            {"nvim-lua/plenary.nvim"}
+        }
     }
-)
+}
 
+-- plugins setup
+local treesitter = require("nvim-treesitter")
+treesitter.setup {
+    ensure_installed = {"rust", "python", "lua", "vim", "vimdoc", "query", "c"},
+    sync_install = false,
+    auto_install = true,
+    ignore_install = {},
+    highlight = {
+        enable = true,
+        additional_vim_regex_highliting = false
+    }
+}
+
+local rosepine = require("rose-pine")
+rosepine.setup {
+    variant = "moon"
+}
+
+-- general keymappings
+vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- telescope keymappings
+local tsin = require("telescope.builtin")
+vim.keymap.set("n", "<leader><leader>", tsin.find_files, {})
+vim.keymap.set("n", "<leader>p", tsin.live_grep, {})
+vim.keymap.set("n", "<leader>b", tsin.buffers, {})
+vim.keymap.set("n", "<leader>h", tsin.help_tags, {})
+vim.keymap.set("n", "<leader>g", tsin.git_files, {})
+vim.keymap.set("n", "<leader>c", tsin.vim_options, {})
+vim.keymap.set("n", "<leader>t", tsin.treesitter, {})
+
+-- lsp setup
 local lsp = require("lsp-zero")
 lsp.preset({})
 lsp.on_attach(
@@ -41,36 +82,43 @@ lsp.on_attach(
         lsp.default_keymaps({buffer = bufnr})
     end
 )
-require("lspconfig").rust_analyzer.setup({})
-require("lspconfig").pyright.setup({})
-require("lspconfig").lua_ls.setup(
-    {
-        settings = {
-            Lua = {
-                runtime = {
-                    version = "LuaJIT"
-                },
-                diagnostics = {
-                    globals = {
-                        "vim",
-                        "require"
-                    }
-                },
-                workspace = {
-                    library = vim.api.nvim_get_runtime_file("", true)
-                },
-                telemetry = {
-                    enable = true
+
+-- used lsps
+require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").pyright.setup {}
+require("lspconfig").lua_ls.setup {
+    settings = {
+        Lua = {
+            runtime = {
+                version = "LuaJIT"
+            },
+            diagnostics = {
+                globals = {
+                    "vim",
+                    "require"
                 }
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false
+            },
+            telemetry = {
+                enable = true
             }
         }
     }
-)
+}
 
-lsp.setup()
+-- enable lsp
+lsp.setup {}
 
+-- general options
 vim.opt.termguicolors = true
-vim.cmd.colorscheme("tokyonight")
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.cmd.colorscheme("rose-pine")
+
+-- auto commands
 vim.api.nvim_create_autocmd(
     "BufWritePost",
     {
