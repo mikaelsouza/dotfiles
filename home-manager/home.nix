0864b6f64e4bab88ee4ca1f5458dfe92b797f0a4
Mikael Souza <mikael.souza.cc@gmail.com>
2023-09-04 10:13:05 -0400

    update home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,42 +1,69 @@
 { config, pkgs, ... }:
 
 {
-  home = {
-    username = "mikael";
-    homeDirectory = "/home/mikael";
-    stateVersion = "23.05";
-    packages = [
-      pkgs.neovim
-      pkgs.gcc
-      pkgs.nodejs
-      pkgs.rustc
-      pkgs.cargo
-      pkgs.rust-analyzer
-      pkgs.rustfmt
-      pkgs.pyright
-      pkgs.black
-      pkgs.lua-language-server
-      pkgs.nodePackages.lua-fmt
-      pkgs.ripgrep
-      pkgs.fd
-    ];
-    file = { 
-    };
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
-    shellAliases = {
-      sw = "home-manager switch";
-      hm = "nvim ~/.config/home-manager/home.nix";
+  home.username = "mikael";
+  home.homeDirectory = "/home/mikael";
+
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  home.packages = [
+    pkgs.python311
+    pkgs.direnv
+    pkgs.neovim
+    pkgs.tmux
+    pkgs.gcc
+    pkgs.nodejs
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.rust-analyzer
+    pkgs.rustfmt
+    pkgs.pyright
+    pkgs.black
+    pkgs.lua-language-server
+    pkgs.nodePackages.lua-fmt
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.nixpkgs-fmt
+    pkgs.lazydocker
+  ];
+
+  home.file = {
+    nvim = {
+      enable = true;
+      source = "/home/mikael/dotfiles/nvim/init.lua";
+      target = ".config/nvim/init.lua";
     };
   };
-  programs = {
-    bash.enable = true;
-    home-manager.enable = true;
-    git = {
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    PATH = "/home/mikael/bin:$PATH";
+  };
+
+  home.shellAliases = {
+    hm = "nvim ~/.config/home-manager/home.nix && home-manager switch";
+  };
+
+  programs.home-manager.enable = true;
+  programs.zsh = {
+    enable = true;
+    oh-my-zsh = {
       enable = true;
-      userName = "Mikael Souza";
-      userEmail = "mikael.souza.cc@gmail.com";
+      theme = "robbyrussell";
+      plugins = [ "git" ];
     };
+  };
+  programs.bash.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Mikael Souza";
+    userEmail = "mikael.souza.cc@gmail.com";
+  };
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
   };
 }
+
